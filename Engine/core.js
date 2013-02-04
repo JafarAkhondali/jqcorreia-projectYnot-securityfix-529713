cf26db4733e0fd06781b76b924afebe10435f7c8
jqcorreia <joseqcorreia@gmail.com>
2013-02-04 22:03:16 +0000

    Unitary tests trying to get the fucking requirejs to work...

--- Engine/core.js
+++ Engine/core.js
@@ -1,25 +1,67 @@
-require(['lib/jquery.min','resources/preloader','resources/tiledmap', 'lib/three.min'], function($,R) {	
-
+define(['lib/jquery.min','resources/preloader','resources/tiledmap', 'lib/three.min'], function($,R) {	
+	console.log("core loading")
 	var base = base || {};
 
 	base.R = R;
 	base.T = THREE;
 
-	window.base = base; // Push base to global namespace
+	base.T.Object3D.prototype.update = function() {
+		
+	}
 
-	// UNIT TESTING
-	var resources = [];
+	base.T.Scene.prototype.update = function() {
+		for(var i in this.children) {
+			this.children[i].update();
+		}
+	}
 
-	console.log("Core unit test");
+	base.renderer = new base.T.WebGLRenderer();
+	base.camera = new base.T.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 10000);
+	base.scene = new base.T.Scene();
 
-	resources['0'] = new R.type.Image("../assets/tileset.png");
-	resources['1'] = new R.type.Data("../assets/area01.tmx");
+	base.start = function() {
+		document.body.appendChild(base.renderer.domElement);
+		base.renderer.setSize(window.innerWidth, window.innerHeight);
+		base.camera.position.z = 20;
+		base.scene.add(base.camera);
+		setInterval(base._loop, 1000 / 60);
+	}
 
-	var posLoad = function() { 
-		console.log("All resources loaded");
-		var map = new base.R.type.TiledMap(resources['1'].obj, resources['0'].obj);
-		console.log(map);
+	base._loop = function() {
+		base.scene.update();
+		base.renderer.render(base.scene, base.camera);
 	};
 
-	new base.R.Loader(resources,posLoad).loadResources();		
+	window.base = base;
+
+	return base;
+	// // UNIT TESTING
+	// var resources = [];
+
+	// console.log("Core unit test");
+
+	// resources['tileset'] = new R.type.Image("../assets/tileset.png");
+	// resources['area'] = new R.type.Data("../assets/area01.tmx");
+
+	// var posLoad = function() { 
+	// 	console.log("All resources loaded");
+	// 	var map = new base.R.type.TiledMap(resources['area'].obj, resources['tileset'].obj);
+	// 	console.log(map);
+	// };
+
+	// new base.R.Loader(resources,posLoad).loadResources();
+
+	// base.start();
+
+	// base.scene.add(new base.T.Mesh(new base.T.CubeGeometry(1,1,1,1,1), new base.T.MeshPhongMaterial({ color: 0x0000FF})));
+
+	// var l = new base.T.PointLight(0xFFFFFF);
+	
+	// l.update = function() {
+	// 	console.log("many times foo");
+	// }
+
+	// l.position.z = 30;
+	// base.scene.add(l);
+	// base.scene.add();
 });
